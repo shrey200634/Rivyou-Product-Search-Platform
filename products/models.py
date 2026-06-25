d823acad570feb986ef809dd3dfa17568486b088
shrey200634 <dshrey60@gmail.com>
2026-06-26 02:13:31 +0530

    add product model in the backend

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255, db_index=True)
+    product_description = models.TextField()
+    category = models.CharField(max_length=100, db_index=True)
+    tags = models.ManyToManyField(Tag, related_name='products', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['category']),
+        ]
+
+    def __str__(self):
+        return self.product_name
